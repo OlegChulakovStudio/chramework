a317552198048527d6efb9d0ebed3f9f6db6a539
Serj Komarovsky <sk@chulakov.ru>
2017-07-17 20:13:24 +0300

    added export of all components to index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,43 @@
 import Avatar from './components/Avatar/Avatar.js';
 import Button from './components/Button/Button.js';
+import Control from './components/Control/Control.js';
+import FileInput from './components/FileInput/FileInput.js';
+import FormCol from './components/FormCol/FormCol.js';
+import FormGroup from './components/FormGroup/FormGroup.js';
+import FormRow from './components/FormRow/FormRow.js';
 import Heading from './components/Heading/Heading.js';
 import Input from './components/Input/Input.js';
 import Logo from './components/Logo/Logo.js';
 import Media from './components/Media/Media.js';
 import Note from './components/Note/Note.js';
 import Preloader from './components/Preloader/Preloader.js';
+import SelectField from './components/SelectField/SelectField.js';
 import Spinner from './components/Spinner/Spinner.js';
+import Table from './components/Table/Table.js';
+import TableCell from './components/TableCell/TableCell.js';
+import TableContent from './components/TableContent/TableContent.js';
+import TableHead from './components/TableHead/TableHead.js';
+import TableRow from './components/TableRow/TableRow.js';
 
 export {
   Avatar,
   Button,
+  Control,
+  FileInput,
+  FormCol,
+  FormGroup,
+  FormRow,
   Heading,
   Input,
   Logo,
   Media,
   Note,
   Preloader,
-  Spinner
+  SelectField,
+  Spinner,
+  Table,
+  TableCell,
+  TableContent,
+  TableHead,
+  TableRow
 }
