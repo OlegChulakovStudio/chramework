2839a1e752423d848cda215483f480d9fc62fc44
Serj Komarovsky <sk@chulakov.ru>
2017-07-25 14:15:41 +0300

    fixed export problems

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -177,6 +177,7 @@ module.exports = {
                     importLoaders: 1,
                     minimize: true,
                     sourceMap: true,
+                    root: '../'
                   },
                 },
                 {
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import Avatar from './components/Avatar/Avatar.js';
 import Button from './components/Button/Button.js';
-import Control from './components/Control/Control.js';
+import Checkbox from './components/Checkbox/Checkbox.js';
 import FileInput from './components/FileInput/FileInput.js';
 import FormCol from './components/FormCol/FormCol.js';
 import FormGroup from './components/FormGroup/FormGroup.js';
@@ -11,6 +11,7 @@ import Logo from './components/Logo/Logo.js';
 import Media from './components/Media/Media.js';
 import Note from './components/Note/Note.js';
 import Preloader from './components/Preloader/Preloader.js';
+import Radio from './components/Radio/Radio.js';
 import SelectField from './components/SelectField/SelectField.js';
 import Spinner from './components/Spinner/Spinner.js';
 import Table from './components/Table/Table.js';
@@ -22,7 +23,7 @@ import TableRow from './components/TableRow/TableRow.js';
 export {
   Avatar,
   Button,
-  Control,
+  Checkbox,
   FileInput,
   FormCol,
   FormGroup,
@@ -33,6 +34,7 @@ export {
   Media,
   Note,
   Preloader,
+  Radio,
   SelectField,
   Spinner,
   Table,
