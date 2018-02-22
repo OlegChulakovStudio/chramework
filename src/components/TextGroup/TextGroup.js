4a2fb40abb3725438447da390f98d690afe6d420
roman_nizhnikov <rn@chulakov.ru>
2018-02-22 22:24:06 +0300

    TextGroup update

--- src/components/TextGroup/TextGroup.js
+++ src/components/TextGroup/TextGroup.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
-import Paragraph from "../Paragraph/Paragraph.js";
 import reactHtmlParser from "react-html-parser";
+
+import Paragraph from "../Paragraph/Paragraph";
+import Link from "../Link/Link";
+
 import './TextGroup.styl';
 
 export default class TextGroup extends Component {
@@ -12,13 +15,13 @@ export default class TextGroup extends Component {
 		text: PropTypes.string,
 		className: PropTypes.string,
 		mod: PropTypes.string,
-		children: PropTypes.any,
+		url: PropTypes.object
 	}
 	static defaultProps = {
-		title: '',
-		subtitle: '',
-		text: '',
-		mod: ''
+		title: undefined,
+		subtitle: undefined,
+		text: undefined,
+		mod: undefined
 	}
 	render() {
 		const {
@@ -28,6 +31,7 @@ export default class TextGroup extends Component {
 			title,
 			subtitle,
 			text,
+			url,
 		} = this.props;
 
 		const TextGroupStyle = classNames('TextGroup', className, {
@@ -37,15 +41,24 @@ export default class TextGroup extends Component {
 		const subtitleMod = mod === "boldTitle" ? "bodySmallGrayItalic" : "subtitle";
 		return (
 			<div className={TextGroupStyle}>
-				{title && <Paragraph mod={titleMod} className="TextGroup__title">
-					{reactHtmlParser(title)}
-				</Paragraph>}
-				{subtitle && <Paragraph mod={subtitleMod} className="TextGroup__subtitle">
-					{reactHtmlParser(subtitle)}
-				</Paragraph>}
-				{text && <Paragraph mod="body" className="TextGroup__text">{reactHtmlParser(text)}</Paragraph>}
-				{children && children}
+				{title &&
+					<Paragraph mod={titleMod} className="TextGroup__title">
+						{url ?
+							<Link {...url}>{reactHtmlParser(title)}</Link> :
+							reactHtmlParser(title)
+						}
+					</Paragraph>
+				}
+				{subtitle &&
+					<Paragraph mod={subtitleMod} className="TextGroup__subtitle">
+						{reactHtmlParser(subtitle)}
+					</Paragraph>
+				}
+				{text &&
+					<Paragraph mod="body" className="TextGroup__text">{reactHtmlParser(text)}</Paragraph>
+				}
+				{children}
 			</div>
-			);
+		);
 	}
 }
