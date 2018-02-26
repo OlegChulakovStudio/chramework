d6ef65c25da0760ea328364a7ee511fbcb9f259f
roman-nizhnikov <rn@chulakov.ru>
2018-02-26 15:07:51 +0300

    index.js update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import './styles/main.styl';
 
+import AchievementsList from './components/AchievementsList/AchievementsList';
 import AppLink from './components/AppLink/AppLink.js';
 import Avatar from './components/Avatar/Avatar.js';
 import Button from './components/Button/Button.js';
-import Checkbox from './components/Checkbox/Checkbox.js';
 import Card from './components/Card/Card.js';
+import Checkbox from './components/Checkbox/Checkbox.js';
+import CheckboxGroup from './components/CheckboxGroup/CheckboxGroup';
+import Clients from './components/Clients/Clients';
 import FileInput from './components/FileInput/FileInput.js';
+import FooterSection from './components/FooterSection/FooterSection';
 import FormCol from './components/FormCol/FormCol.js';
 import FormGroup from './components/FormGroup/FormGroup.js';
 import FormRow from './components/FormRow/FormRow.js';
@@ -15,10 +19,14 @@ import Input from './components/Input/Input.js';
 import Logo from './components/Logo/Logo.js';
 import Media from './components/Media/Media.js';
 import Note from './components/Note/Note.js';
+import Paragraph from './components/Paragraph/Paragraph';
+import Player from './components/Player/Player';
 import Preloader from './components/Preloader/Preloader.js';
 import Radio from './components/Radio/Radio.js';
 import SelectField from './components/SelectField/SelectField.js';
+import ServicesList from './components/ServicesList/ServicesList';
 import Spinner from './components/Spinner/Spinner.js';
+import Summary from './components/Summary/Summary';
 import Table from './components/Table/Table.js';
 import TableCell from './components/TableCell/TableCell.js';
 import TableContent from './components/TableContent/TableContent.js';
@@ -27,33 +35,45 @@ import TableRow from './components/TableRow/TableRow.js';
 import Slider from './components/Slider/Slider.js';
 import Reviews from './components/Reviews/Reviews.js';
 import TextGroup from './components/TextGroup/TextGroup.js';
+import VideoBlock from './components/VideoBlock/VideoBlock';
+import WorkItem from './components/WorkItem/WorkItem';
 
 export {
-  AppLink,
-  Avatar,
-  Button,
-  Checkbox,
-  Card,
-  FileInput,
-  FormCol,
-  FormGroup,
-  FormRow,
-  Heading,
-  Icon,
-  Input,
-  Logo,
-  Media,
-  Note,
-  Preloader,
-  Radio,
-  SelectField,
-  Spinner,
-  Table,
-  TableCell,
-  TableContent,
-  TableHeader,
-  TableRow,
-  Slider,
-  TextGroup,
-  Reviews
+	AchievementsList,
+	AppLink,
+	Avatar,
+	Button,
+	Card,
+	Checkbox,
+	CheckboxGroup,
+	Clients,
+	FileInput,
+	FooterSection,
+	FormCol,
+	FormGroup,
+	FormRow,
+	Heading,
+	Icon,
+	Input,
+	Logo,
+	Media,
+	Note,
+	Paragraph,
+	Player,
+	Preloader,
+	Radio,
+	SelectField,
+	ServicesList,
+	Spinner,
+	Summary,
+	Table,
+	TableCell,
+	TableContent,
+	TableHeader,
+	TableRow,
+	Slider,
+	TextGroup,
+	Reviews,
+	VideoBlock,
+	WorkItem
 };
