9b6a1809d448cb5da25371709fb04088c84143a0
roman_nizhnikov <rn@chulakov.ru>
2018-03-27 20:28:59 +0300

    navigator test

--- src/utils/devices.js
+++ src/utils/devices.js
@@ -7,15 +7,42 @@ export const isPad = () => {
 export const isMobile = () => {
 	return typeof window !== 'undefined' ? window.innerWidth <= 1024 : false;
 };
-export const isAndroid = () => /(android)/i.test(navigator.userAgent);
-export const isIos = () => /(iPad|iPhone|iPod touch)/i.test(navigator.userAgent);
-export const isIphone = () => /(iPhone|iPod touch)/i.test(navigator.userAgent);
-export const isIpad = () => /iPad/i.test(navigator.userAgent);
+export const isAndroid = () => {
+	console.log('- - - - - - - - - - -');
+	console.log('chramework', navigator.userAgent);
+	console.log('- - - - - - - - - - -');
+	return /(android)/i.test(navigator.userAgent);
+}
+export const isIos = () => {
+	const navigator = typeof window !== 'undefined' ? window.navigator : global.navigator;
+	console.log('- - - - - - - - - - -');
+	console.log('isIos chramework', navigator.userAgent);
+	console.log('- - - - - - - - - - -');
+	return /(iPad|iPhone|iPod touch)/i.test(navigator.userAgent);
+}
+export const isIphone = () => {
+	const navigator = typeof window !== 'undefined' ? window.navigator : global.navigator;
+	console.log('- - - - - - - - - - -');
+	console.log('isIphone chramework', navigator.userAgent);
+	console.log('- - - - - - - - - - -');
+	return /(iPhone|iPod touch)/i.test(navigator.userAgent);
+}
+export const isIpad = () => {
+	const navigator = typeof window !== 'undefined' ? window.navigator : global.navigator;
+	console.log('- - - - - - - - - - -');
+	console.log('isIpad chramework', navigator.userAgent);
+	console.log('- - - - - - - - - - -');
+	return /iPad/i.test(navigator.userAgent);
+}
 // export const isMac = () => (/Mac/i).test(navigator.platform);
 
 export const isFirefox = () => (/firefox/i).test(navigator.userAgent);
 
 export const iosVersion = () => {
+	const navigator = typeof window !== 'undefined' ? window.navigator : global.navigator;
+	console.log('- - - - - - - - - - -');
+	console.log('iosVersion chramework', navigator.userAgent);
+	console.log('- - - - - - - - - - -');
 	if (isIos() && typeof window !== 'undefined') {
 		const v = navigator.appVersion.match(/OS (\d+)_(\d+)_?(\d+)?/);
 		if (v !== null) {
