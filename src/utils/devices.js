08d7f5a9aeed3c48c43e243a5307658dbb12f502
roman_nizhnikov <rn@chulakov.ru>
2018-03-27 20:59:15 +0300

    devices fix

--- src/utils/devices.js
+++ src/utils/devices.js
@@ -1,3 +1,9 @@
+const getNavigator = () => {
+	const navigator = typeof window !== 'undefined' ? window.navigator : global.navigator
+	if (!navigator) console.log('navigator undefined');
+	return navigator;
+}
+
 export const isPhone = () => {
 	return typeof window !== 'undefined' ? window.innerWidth <= 667 : false;
 };
@@ -8,43 +14,29 @@ export const isMobile = () => {
 	return typeof window !== 'undefined' ? window.innerWidth <= 1024 : false;
 };
 export const isAndroid = () => {
-	console.log('- - - - - - - - - - -');
-	console.log('chramework', navigator.userAgent);
-	console.log('- - - - - - - - - - -');
-	return /(android)/i.test(navigator.userAgent);
+	const navigator = getNavigator();
+	return navigator ? /(android)/i.test(navigator.userAgent) : false;
 }
 export const isIos = () => {
-	const navigator = typeof window !== 'undefined' ? window.navigator : global.navigator;
-	console.log('- - - - - - - - - - -');
-	console.log('isIos chramework', navigator.userAgent);
-	console.log('- - - - - - - - - - -');
-	return /(iPad|iPhone|iPod touch)/i.test(navigator.userAgent);
+	const navigator = getNavigator();
+	return navigator ? /(iPad|iPhone|iPod touch)/i.test(navigator.userAgent) : false;
 }
 export const isIphone = () => {
-	const navigator = typeof window !== 'undefined' ? window.navigator : global.navigator;
-	console.log('- - - - - - - - - - -');
-	console.log('isIphone chramework', navigator.userAgent);
-	console.log('- - - - - - - - - - -');
-	return /(iPhone|iPod touch)/i.test(navigator.userAgent);
+	const navigator = getNavigator();
+	return navigator ? /(iPhone|iPod touch)/i.test(navigator.userAgent) : false;
 }
 export const isIpad = () => {
-	const navigator = typeof window !== 'undefined' ? window.navigator : global.navigator;
-	console.log('- - - - - - - - - - -');
-	console.log('isIpad chramework', navigator.userAgent);
-	console.log('- - - - - - - - - - -');
-	return /iPad/i.test(navigator.userAgent);
+	const navigator = getNavigator();
+	return navigator ? /iPad/i.test(navigator.userAgent) : false;
 }
 // export const isMac = () => (/Mac/i).test(navigator.platform);
 
 export const isFirefox = () => (/firefox/i).test(navigator.userAgent);
 
 export const iosVersion = () => {
-	const navigator = typeof window !== 'undefined' ? window.navigator : global.navigator;
-	console.log('- - - - - - - - - - -');
-	console.log('iosVersion chramework', navigator.userAgent);
-	console.log('- - - - - - - - - - -');
+	const navigator = getNavigator();
 	if (isIos() && typeof window !== 'undefined') {
-		const v = navigator.appVersion.match(/OS (\d+)_(\d+)_?(\d+)?/);
+		const v = navigator ? navigator.appVersion.match(/OS (\d+)_(\d+)_?(\d+)?/) : null;
 		if (v !== null) {
 			return parseInt(v[1], 10);
 		}
