c86debad74960458251f263d9c35340d7ace66ab
igorbarzakh <igorbarzakh@iMac-Maksim-2.local>
2019-07-05 17:18:25 +0300

    Added new logos

--- src/components/Clients/Clients.js
+++ src/components/Clients/Clients.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import classNames from "classnames";
 import Link from "../Link/Link";
-import Demo from "../../assets/clients/mts.svg";
+import Demo from "../../assets/clients/sevenFlowers.svg";
 import "./Clients.styl";
 
 const filterDemo = {
