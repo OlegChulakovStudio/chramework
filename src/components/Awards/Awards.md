8e64a3b80f3fd31c1de04a246c7bb6c38c1ea2e1
Aleksandr <alxndr.boev@gmail.com>
2019-10-24 17:32:52 +0300

    add Awwards;

---
+++ src/components/Awards/Awards.md
@@ -0,0 +1,55 @@
+Awards:
+
+    <Awards id="awwards"
+        data={[
+            {
+                "type": "eda",
+                "text": "EDA",
+                "total": "× 3"
+            },
+            {
+                "type": "fwa",
+                "text": "FWA",
+                "total": "× 3"
+            },
+            {
+                "type": "awwwards",
+                "text": "AWWWARDS",
+                "total": "× 10"
+            },
+            {
+                "type": "cssda",
+                "text": "CSSDA",
+                "total": "× 11"
+            },
+            {
+                "type": "1",
+                "number": true,
+                "text": "дизайн-студия &#8470;&nbsp;1&nbsp;в&nbsp;России*"
+            },
+            {
+                "type": "4",
+                "number": true,
+                "text": "место digital production*"
+            },
+            {
+                "type": "5",
+                "number": true,
+                "text": "Олег Чулаков в&nbsp;топ-5&nbsp;персон**"
+            },
+            {
+                "type": "10",
+                "number": true,
+                "text": "лет опыта"
+            }
+        ]}
+        footnotes={[
+            {
+                "text": "По версии Tagline",
+                "tagline": true
+            },
+            {
+                "text": "Рейтинг ключевых персон российского рынка digital-маркетинга и&nbsp;веб-разработки 2018 по&nbsp;версии Tagline",
+                "keyperson": true
+            }
+        ]} />
--- src/index.js
+++ src/index.js
@@ -1,108 +1,110 @@
-import "./styles/main.styl";
+import './styles/main.styl';
 
-import AchievementsList from "./components/AchievementsList/AchievementsList";
-import AppLink from "./components/AppLink/AppLink.js";
-import Avatar from "./components/Avatar/Avatar.js";
-import Button from "./components/Button/Button.js";
-import Card from "./components/Card/Card.js";
-import Control from "./components/Control/Control.js";
-import Checkbox from "./components/Checkbox/Checkbox.js";
-import CheckboxGroup from "./components/CheckboxGroup/CheckboxGroup";
-import Clients from "./components/Clients/Clients";
-import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary";
-import FileInput from "./components/FileInput/FileInput.js";
-import FooterSection from "./components/FooterSection/FooterSection";
-import FormCol from "./components/FormCol/FormCol.js";
-import FormGroup from "./components/FormGroup/FormGroup.js";
-import FormRow from "./components/FormRow/FormRow.js";
-import Heading from "./components/Heading/Heading.js";
-import Icon from "./components/Icon/Icon.js";
-import Input from "./components/Input/Input.js";
-import Link from "./components/Link/Link.js";
-import Logo from "./components/Logo/Logo.js";
-import Media from "./components/Media/Media.js";
-import Note from "./components/Note/Note.js";
-import Paragraph from "./components/Paragraph/Paragraph";
-import Player from "./components/Player/Player";
-import Preloader from "./components/Preloader/Preloader.js";
-import Radio from "./components/Radio/Radio.js";
-import SelectField from "./components/SelectField/SelectField.js";
-import ServicesList from "./components/ServicesList/ServicesList";
-import Spinner from "./components/Spinner/Spinner.js";
-import Summary from "./components/Summary/Summary";
-import Table from "./components/Table/Table.js";
-import TableCell from "./components/TableCell/TableCell.js";
-import TableContent from "./components/TableContent/TableContent.js";
-import TableHeader from "./components/TableHeader/TableHeader.js";
-import TableRow from "./components/TableRow/TableRow.js";
-import Slider from "./components/Slider/Slider.js";
-import Reviews from "./components/Reviews/Reviews.js";
-import TextGroup from "./components/TextGroup/TextGroup.js";
-import VideoBlock from "./components/VideoBlock/VideoBlock";
-import WorkItem from "./components/WorkItem/WorkItem";
-import Block from "./components/Block/Block";
-import SocialNetworks from "./components/SocialNetworks/SocialNetworks";
-import FooterMini from "./components/FooterMini/FooterMini";
-import Header from "./components/Header/Header";
-import MenuInner from "./components/Header/MenuInner";
-import Hamburger from "./components/Header/Hamburger";
-import WorkTags from "./components/WorkTags/WorkTags";
-import HeaderWrap from "./components/HeaderWrap/HeaderWrap";
-import ui from "./components/modules/ui";
-import modal from "./components/modules/modal";
+import AchievementsList from './components/AchievementsList/AchievementsList';
+import Awards from './components/Awards/Awards.js';
+import AppLink from './components/AppLink/AppLink.js';
+import Avatar from './components/Avatar/Avatar.js';
+import Button from './components/Button/Button.js';
+import Card from './components/Card/Card.js';
+import Control from './components/Control/Control.js';
+import Checkbox from './components/Checkbox/Checkbox.js';
+import CheckboxGroup from './components/CheckboxGroup/CheckboxGroup';
+import Clients from './components/Clients/Clients';
+import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
+import FileInput from './components/FileInput/FileInput.js';
+import FooterSection from './components/FooterSection/FooterSection';
+import FormCol from './components/FormCol/FormCol.js';
+import FormGroup from './components/FormGroup/FormGroup.js';
+import FormRow from './components/FormRow/FormRow.js';
+import Heading from './components/Heading/Heading.js';
+import Icon from './components/Icon/Icon.js';
+import Input from './components/Input/Input.js';
+import Link from './components/Link/Link.js';
+import Logo from './components/Logo/Logo.js';
+import Media from './components/Media/Media.js';
+import Note from './components/Note/Note.js';
+import Paragraph from './components/Paragraph/Paragraph';
+import Player from './components/Player/Player';
+import Preloader from './components/Preloader/Preloader.js';
+import Radio from './components/Radio/Radio.js';
+import SelectField from './components/SelectField/SelectField.js';
+import ServicesList from './components/ServicesList/ServicesList';
+import Spinner from './components/Spinner/Spinner.js';
+import Summary from './components/Summary/Summary';
+import Table from './components/Table/Table.js';
+import TableCell from './components/TableCell/TableCell.js';
+import TableContent from './components/TableContent/TableContent.js';
+import TableHeader from './components/TableHeader/TableHeader.js';
+import TableRow from './components/TableRow/TableRow.js';
+import Slider from './components/Slider/Slider.js';
+import Reviews from './components/Reviews/Reviews.js';
+import TextGroup from './components/TextGroup/TextGroup.js';
+import VideoBlock from './components/VideoBlock/VideoBlock';
+import WorkItem from './components/WorkItem/WorkItem';
+import Block from './components/Block/Block';
+import SocialNetworks from './components/SocialNetworks/SocialNetworks';
+import FooterMini from './components/FooterMini/FooterMini';
+import Header from './components/Header/Header';
+import MenuInner from './components/Header/MenuInner';
+import Hamburger from './components/Header/Hamburger';
+import WorkTags from './components/WorkTags/WorkTags';
+import HeaderWrap from './components/HeaderWrap/HeaderWrap';
+import ui from './components/modules/ui';
+import modal from './components/modules/modal';
 
-import AchievementsData from "./components/AchievementsList/data.json";
+import AchievementsData from './components/AchievementsList/data.json';
 
 export {
-  AchievementsList,
-  AchievementsData,
-  AppLink,
-  Avatar,
-  Button,
-  Card,
-  Checkbox,
-  CheckboxGroup,
-  Clients,
-  ErrorBoundary,
-  FileInput,
-  FooterSection,
-  FormCol,
-  FormGroup,
-  FormRow,
-  Heading,
-  Icon,
-  Input,
-  Link,
-  Logo,
-  Media,
-  Note,
-  Paragraph,
-  Player,
-  Preloader,
-  Radio,
-  SelectField,
-  ServicesList,
-  Spinner,
-  Summary,
-  Table,
-  TableCell,
-  TableContent,
-  TableHeader,
-  TableRow,
-  Slider,
-  TextGroup,
-  Reviews,
-  VideoBlock,
-  WorkItem,
-  Control,
-  SocialNetworks,
-  FooterMini,
-  Block,
-  Header,
-  HeaderWrap,
-  Hamburger,
-  ui,
-  modal,
-  WorkTags,
-  MenuInner
+	AchievementsList,
+	AchievementsData,
+	AppLink,
+	Avatar,
+	Awards,
+	Button,
+	Card,
+	Checkbox,
+	CheckboxGroup,
+	Clients,
+	ErrorBoundary,
+	FileInput,
+	FooterSection,
+	FormCol,
+	FormGroup,
+	FormRow,
+	Heading,
+	Icon,
+	Input,
+	Link,
+	Logo,
+	Media,
+	Note,
+	Paragraph,
+	Player,
+	Preloader,
+	Radio,
+	SelectField,
+	ServicesList,
+	Spinner,
+	Summary,
+	Table,
+	TableCell,
+	TableContent,
+	TableHeader,
+	TableRow,
+	Slider,
+	TextGroup,
+	Reviews,
+	VideoBlock,
+	WorkItem,
+	Control,
+	SocialNetworks,
+	FooterMini,
+	Block,
+	Header,
+	HeaderWrap,
+	Hamburger,
+	ui,
+	modal,
+	WorkTags,
+	MenuInner
 };
